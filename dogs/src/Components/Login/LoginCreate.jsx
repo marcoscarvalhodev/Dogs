9f1ddbbd96e59ea53c217620f40f8bcfeac58c92
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-07-19 01:21:04 -0300

    useFetch hook was added for scalability purposes and reuse of this functionality

--- dogs/src/Components/Login/LoginCreate.jsx
+++ dogs/src/Components/Login/LoginCreate.jsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import Input from '../Forms/Input';
-import Button from '../Forms/Button';
-import useForm from '../../Hooks/useForm'
-import { USER_POST } from '../../api';
-import { UserContext } from '../../UserContext';
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import { USER_POST } from "../../api";
+import { UserContext } from "../../UserContext";
+import useFetch from "../../Hooks/useFetch";
+import Error from '../Helper/Error'
 
 const LoginCreate = () => {
-
   const username = useForm();
-  const email = useForm('email');
-  const password = useForm('password');
+  const email = useForm("email");
+  const password = useForm("password");
 
-  const {userLogin} = React.useContext(UserContext)
+  const { userLogin } = React.useContext(UserContext);
+  const {loading, error, request} = useFetch()
 
   async function handleSubmit(event) {
     event.preventDefault();
-    const {url, options} = USER_POST({
+    const { url, options } = USER_POST({
       username: username.value,
       email: email.value,
-      password: password.value
-    })
-    const response = await fetch(url, options)
-    if(response.ok) userLogin(username.value, password.value)
+      password: password.value,
+    });
+    const {response} = await request(url, options);
+    if (response.ok) userLogin(username.value, password.value);
   }
 
   return (
     <section className="animeLeft">
       <h1 className="title">Sign Up</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="User" type="text" name="username" {...username}/>
-        <Input label="Email" type="email" name="email" {...email}/>
-        <Input label="Password" type="password" name="password" {...password}/>
-        <Button>Sign Up</Button>
+        <Input label="User" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Password" type="password" name="password" {...password} />
+        {loading ? <Button disabled>Signing Up...</Button> : <Button>Sign Up</Button>}
+        <Error error={error}/>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default LoginCreate
+export default LoginCreate;
