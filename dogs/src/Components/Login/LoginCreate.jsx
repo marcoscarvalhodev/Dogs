4690c667712bb910691ed3436f4b7c557c582469
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-07-19 01:03:10 -0300

    sign up section and its functionalities were implemented

--- dogs/src/Components/Login/LoginCreate.jsx
+++ dogs/src/Components/Login/LoginCreate.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm'
+import { USER_POST } from '../../api';
+import { UserContext } from '../../UserContext';
 
 const LoginCreate = () => {
+
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm('password');
+
+  const {userLogin} = React.useContext(UserContext)
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const {url, options} = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value
+    })
+    const response = await fetch(url, options)
+    if(response.ok) userLogin(username.value, password.value)
+  }
+
   return (
-    <div>LoginCreate</div>
+    <section className="animeLeft">
+      <h1 className="title">Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="User" type="text" name="username" {...username}/>
+        <Input label="Email" type="email" name="email" {...email}/>
+        <Input label="Password" type="password" name="password" {...password}/>
+        <Button>Sign Up</Button>
+      </form>
+    </section>
   )
 }
 
