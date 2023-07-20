84a63c2647b58c31780ccff1bf2db879300aa97e
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-07-20 08:07:44 -0300

    protected route was added to prevent a non-logged in user to access an area from the application

--- dogs/src/Hooks/useFetch.js
+++ dogs/src/Hooks/useFetch.js
@@ -14,7 +14,6 @@ const useFetch = () => {
       response = await fetch(url, options);
       json = await response.json();
       if(!response.ok) {
-        throw new Error(json.message)
       }
     } catch(err) {
       json = null;
