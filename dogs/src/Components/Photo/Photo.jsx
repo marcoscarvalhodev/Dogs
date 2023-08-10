37d58b4d5259059948328cee768bed0a2a728151
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-08-10 02:11:31 -0300

    little fixes in single page photo styling and comments

--- dogs/src/Components/Photo/Photo.jsx
+++ dogs/src/Components/Photo/Photo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import useFetch from "../../Hooks/useFetch";
-import { PHOTO_PAGE_GET } from "../../api";
+import { PHOTO_GET } from "../../api";
 import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
 import PhotoContent from "./PhotoContent";
@@ -11,7 +11,7 @@ const Photo = () => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
-    const { url, options } = PHOTO_PAGE_GET(id);
+    const { url, options } = PHOTO_GET(id);
     request(url, options);
   }, [request, id]);
 
