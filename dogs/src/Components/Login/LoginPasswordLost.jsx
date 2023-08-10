dc13b31c2bd2276acb314ec26e38638f7b36db94
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-08-09 23:51:59 -0300

    lost password implemented in the application

--- dogs/src/Components/Login/LoginPasswordLost.jsx
+++ dogs/src/Components/Login/LoginPasswordLost.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
+import { PASSWORD_LOST } from "../../api";
+import Error from "../Helper/Error";
 
 const LoginPasswordLost = () => {
-  return (
-    <div>LoginPasswordLost</div>
-  )
-}
+  const login = useForm();
+  const { data, loading, error, request } = useFetch();
 
-export default LoginPasswordLost
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace("lost", "reset"),
+      });
+      request(url, options);
+    }
+  }
+
+  return (
+    <section>
+      <h1 className="title">Forgot password?</h1>
+      {data ? (
+        <p style={{color: '#4c1'}}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / User" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Sending...</Button>
+          ) : (
+            <Button>Send Email</Button>
+          )}
+        </form>
+      )}
+      <Error error={error} />
+    </section>
+  );
+};
+
+export default LoginPasswordLost;
